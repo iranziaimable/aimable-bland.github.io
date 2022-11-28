54938257598b9405e586f0703ee7ca24503ee955
Aimable Iranzi <iranziaimable05@gmail.com>
2022-11-28 10:29:29 +0200

    Some Modification on article

--- js/app.js
+++ js/app.js
@@ -7,3 +7,16 @@ window.addEventListener("scrolled", () => {
     nav.classList.remove("scrolled");
   }
 });
+
+function addArticle() {
+  let tbl = document.getElementById("myArticle");
+  let row = tbl.insertRow();
+  let cell1 = row.insertCell();
+  let cell2 = row.insertCell();
+  let cell3 = row.insertCell();
+  let cell4 = row.insertCell();
+  cell1.innerHTML = "Bonus";
+  cell2.innerHTML = "bonne anne";
+  cell3.innerHTML = "21-12-2022";
+  cell4.innerHTML = "No image yet";
+}
